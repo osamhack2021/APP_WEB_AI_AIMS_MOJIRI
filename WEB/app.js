6d9492b52bb33d96377f7faf84c7578f9750c085
RohSuIn <bigpie1367@gmail.com>
2021-09-29 13:31:52 +0000

    test signUp

--- WEB/app.js
+++ WEB/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 // router
 var indexRouter = require('./routes/index');
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- WEB/routes/users.js
+++ WEB/routes/users.js
@@ -5,20 +5,39 @@ var connection = mysql.createConnection(dbconfig);
 
 var router = express.Router();
 
+// create user
+// 앱상에서 온 가입 요청을 확인한 뒤 DB에 추가
+router.post('/signUp', (req, res) => {
+    console.log(req.body);  // for test
+    res.send("success");
+});
+
+// read user
+// 관리자 페이지에서 유저들을 조회하기 위함
+// router.get('/', (req, res) => {
+//     console.log("get access");
+
+// });
+
+// update user
+// 해당 User의 권한을 변경하기 위함
+router.put('/:serial_num', (req, res) => {
+    console.log("update access");
+});
+
+// delete user
+// 해당 사용자 삭제
+router.delete('/:serial_num', (req, res) => {
+    console.log("delete access");
+})
+
+// DB connection test
 router.get('/', (req, res) => {
     connection.query('SELECT * from Users', (error, rows) => {
         if (error) throw error;
         console.log('User info is : ', rows);
         res.send(rows);
     });
-});
-
-router.get('/signIn', (req, res) => {
-    console.log('hello signIn');
-})
-
-router.post('/signIn', (req, res) => {
-    console.log('hello signin');
 });
 
 module.exports = router;
