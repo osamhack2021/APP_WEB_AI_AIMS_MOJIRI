a33c02be12ffd75bb8d147fa3a22d240f1eb9151
RohSuIn <bigpie1367@gmail.com>
2021-10-09 02:57:52 +0000

    mkdir frontend

--- WEB/backend/routes/index.js
+++ WEB/backend/routes/index.js
@@ -5,13 +5,9 @@ const path = require('path')
 var accountController = require('../controllers/accountController');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { error: false });
-// });
-
-router.get('/', function(req, res, err) {
-  res.sendFile(path.join(__dirname + "/../views/index.html"));
-})
+router.get('/', function(req, res, next) {
+  res.render('pages/dashboard');
+});
 
 router.post('/signUp', accountController.signUp);
 router.post('/signIn', accountController.signIn);
