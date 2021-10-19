3130bb7d3988b87b543486871c40b1c79969dd1d
RohSuIn <bigpie1367@gmail.com>
2021-10-19 20:13:16 +0900

    Update pictureController.js

--- WEB/controllers/pictureController.js
+++ WEB/controllers/pictureController.js
@@ -9,7 +9,7 @@ exports.create = async (req, res) => {
     fs.writeFileSync('./temp/target.jpg', decodeImg);
 
     // run AI code 
-    await exec('/home/osam22/APP_WEB_AI_AIMS_MOJIRI/AI/run.sh /home/osam22/APP_WEB_AI_AIMS_MOJIRI/WEB/target.jpg', (error, stdout, stderr) => {
+    await exec('/home/osam22/APP_WEB_AI_AIMS_MOJIRI/AI/run.sh /home/osam22/APP_WEB_AI_AIMS_MOJIRI/WEB/temp/target.jpg', (error, stdout, stderr) => {
         if (error !== null) {
             console.log(`exec error: ${error}`);
         } else {
