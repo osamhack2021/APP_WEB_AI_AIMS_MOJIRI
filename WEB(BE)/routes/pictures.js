25d11d8d55e8edea155c4d78f9262f59a68e1bee
RohSuIn <bigpie1367@gmail.com>
2021-10-20 22:30:51 +0900

    Delete WEB(BE) directory

--- WEB(BE)/routes/pictures.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var pictureController = require('../controllers/pictureController');
-
-router.post('/create', pictureController.create);
-
-module.exports = router;
